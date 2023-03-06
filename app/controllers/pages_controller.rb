b3a374664d24a7b55db033deff3798d29dcff932
OsMax43 <109098127+OsMax43@users.noreply.github.com>
2023-03-06 19:57:18 +0200

    pages controller fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def index
+    current_date = Date.today
+    month_start = current_date.beginning_of_month
+    @outgo = Operation.where("odate BETWEEN ? AND ? ", month_start, current_date).where("otype = ? ", "витрата").sum(:amount)
+    @income = Operation.where("odate BETWEEN ? AND ? ", month_start, current_date).where("otype = ? ", "доход").sum(:amount)
   end
 end
